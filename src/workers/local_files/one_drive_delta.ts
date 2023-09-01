48071807f41655d9d4b8243bbe5adb600b1f97c8
Rafael Medeiros <rafael.medeiros@carenet.com.br>
2023-09-01 01:33:56 -0300

    🚧

--- src/workers/local_files/one_drive_delta.ts
+++ src/workers/local_files/one_drive_delta.ts
@@ -10,21 +10,25 @@ export type NotifyLocalFile = (fileName: string) => void;
 
 export class OneDrive {
   private readonly oneDriveDAO: OneDriveDAO;
+  private readonly cache: Map<string, string>;
 
   public constructor(oneDriveDAO: OneDriveDAO) {
     this.oneDriveDAO = oneDriveDAO;
+    this.cache = new Map();
+    this.scheduleCacheClean();
   }
 
   private async identify(fileId: string, driveId: string): Promise<string> {
     let fileName: string = undefined;
 
+    if (this.cache.has(fileId)) return this.cache.get(fileId);
+
     const children: OneDriveChildren = await this.oneDriveDAO.getChildren(driveId, fileId);
-
     const entriesFile: OneDriveItem = (children.value?.filter((f: OneDriveItem) => f.name === 'entries.json') || [undefined])[0];
-
     if (entriesFile) {
       const content: Resource = (await this.oneDriveDAO.getFileContent(driveId, entriesFile.id)) as Resource;
       fileName = content?.resource;
+      this.cache.set(fileId, fileName);
     }
 
     return fileName;
@@ -43,6 +47,19 @@ export class OneDrive {
         await waitFor(1000);
       }
     }
+  }
+
+  private scheduleCacheClean(): void {
+    const d: Date = new Date();
+    d.setDate(d.getDate() + 1);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    d.setMilliseconds(0);
+
+    setTimeout(() => {
+      this.cache.clear();
+      this.scheduleCacheClean();
+    }, d.getTime() - Date.now());
   }
 
   public async delta(driveId: string, delta: string, notify: NotifyLocalFile): Promise<string> {
