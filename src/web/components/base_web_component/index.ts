aa5d48df407e257084ec1a3ce24b1512d4b96cc0
Rafael Medeiros <rafael.medeiros@carenet.com.br>
2023-09-08 04:09:02 -0300

    🚧

--- src/web/components/base_web_component/index.ts
+++ src/web/components/base_web_component/index.ts
@@ -4,24 +4,18 @@ import path from 'path';
 type Prototype = { constructor: { name: string } };
 
 export class BaseWebComponent {
-  private readonly regex: RegExp = new RegExp(/{{(.*?)}}/g);
+  private readonly contentRegex: RegExp = new RegExp(/{{(.*?)}}/g);
+  private readonly pathRegex: RegExp = process.platform === 'win32' ? new RegExp(/\((.*?)\\([^\\]*)\)/) : new RegExp(/\((.*?)\/([^/]*)\)/);
   private readonly content: string;
 
   public constructor() {
     const className: string = (Object.getPrototypeOf(this) as Prototype).constructor.name;
     const stackLine: string = new Error().stack.split('\n').filter((l: string) => l.startsWith(`    at new ${className}`))[0];
-    console.info('--------------------');
-    console.info(new Error().stack.split('\n').filter((l: string) => l.startsWith(`    at new ${className}`)));
-    console.info('--------------------');
-    console.info(stackLine);
-    console.info('--------------------');
-    console.info(stackLine.match(/\((.*?)\\([^\\]*)\)/));
-    console.info('--------------------');
-    this.content = fs.readFileSync(path.join(stackLine.match(/\((.*?)\\([^\\]*)\)/)[1], 'index.html')).toString();
+    this.content = fs.readFileSync(path.join(stackLine.match(this.pathRegex)[1], 'index.html')).toString();
   }
 
   protected getContent(variables: Record<string, string>): string {
-    return this.content.replace(this.regex, (_: string, ...args: Array<string>): string => {
+    return this.content.replace(this.contentRegex, (_: string, ...args: Array<string>): string => {
       return variables[args[0]];
     });
   }
