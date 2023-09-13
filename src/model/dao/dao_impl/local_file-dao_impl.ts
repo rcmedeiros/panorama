be78392e432a3aae695767dcd4e3e8d07b611e65
Rafael Medeiros <rafael.medeiros@carenet.com.br>
2023-09-13 02:20:41 -0300

    🚧

--- src/model/dao/dao_impl/local_file-dao_impl.ts
+++ src/model/dao/dao_impl/local_file-dao_impl.ts
@@ -4,10 +4,12 @@ import { BaseDbDAOImpl } from './base_db-dao_impl';
 
 export class LocalFileDAOImpl extends BaseDbDAOImpl implements LocalFileDAO {
   public async insertFile(file: LocalFile): Promise<void> {
-    await this.db.executeQuery({
-      name: 'insertLocalFiles',
-      text: 'INSERT INTO main.local_file ("member", file_name, project, timestamp) VALUES($1, $2, $3, $4);',
-      values: [file.member, file.fileName, file.project, file.timestamp],
-    });
+    if (file.project) {
+      await this.db.executeQuery({
+        name: 'insertLocalFiles',
+        text: 'INSERT INTO main.local_file ("member", file_name, project, timestamp) VALUES($1, $2, $3, $4);',
+        values: [file.member, file.fileName, file.project, file.timestamp],
+      });
+    }
   }
 }
